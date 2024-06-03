59cecfeec3156980333523ae85cf90d1b5ea6bf3
Benjamin Legrand <b.legrand@groupeonepoint.com>
2024-06-03 12:55:28 +0200

    feat(i18n): add transloco

--- src/app/pages/home/components/home-page/home-page.component.ts
+++ src/app/pages/home/components/home-page/home-page.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
-  template: `
-    <h2>Bienvenue</h2>
+  imports: [TranslocoDirective],
+  template: `<ng-container *transloco="let t">
+    <h2>{{ t('home.welcome') }}</h2>
     <p>home-page works!</p>
-  `,
+  </ng-container> `,
   styles: `
     :host {
       display: flex;
