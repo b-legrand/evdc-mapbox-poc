c31bcbd35c6b418d9e82fcb35db784bf46c8bdf0
Benjamin Legrand <b.legrand@groupeonepoint.com>
2024-06-03 12:55:28 +0200

    feat(i18n): add transloco

--- src/app/supported-lang.guard.ts
+++ src/app/supported-lang.guard.ts
@@ -1,6 +1,14 @@
+import { inject } from '@angular/core';
 import { CanMatchFn } from '@angular/router';
+import { TranslocoService } from '@jsverse/transloco';
 
 export const supportedLangGuard: CanMatchFn = (_route, segments) => {
   const lang = segments[0].path;
-  return ['fr', 'en'].includes(lang);
+  const supportedLand = ['fr', 'en', 'de', 'it', 'es', 'nl'].includes(lang);
+  if (!supportedLand) {
+    return false;
+  }
+  const transloco = inject(TranslocoService);
+  transloco.setActiveLang(lang);
+  return true;
 };
